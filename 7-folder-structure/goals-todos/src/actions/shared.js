93bcc9fc2caa97e22e51be1874e8fd7373258a4d
MFAC <ninafroes@gmail.com>
2020-02-02 15:18:15 +0100

    feat: Add shared actions file

---
+++ 7-folder-structure/goals-todos/src/actions/shared.js
@@ -0,0 +1,21 @@
+import API from 'goals-todos-api'
+
+// ACTION TYPE
+const RECEIVE_DATA = 'RECEIVE_DATA'
+
+// ACTION CREATOR
+const receiveData = (todos, goals) => ({
+  type: RECEIVE_DATA,
+  todos,
+  goals
+})
+
+export const handleInitialData = () => {
+  return dispatch => {
+    return Promise.all([API.fetchTodos(), API.fetchGoals()]).then(
+      ([todos, goals]) => {
+        dispatch(receiveData(todos, goals))
+      }
+    )
+  }
+}
