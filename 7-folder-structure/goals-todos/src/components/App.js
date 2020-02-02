ae1788c8dc4c49a17f62a10893e0aaea07a4a095
MFAC <ninafroes@gmail.com>
2020-02-02 15:57:02 +0100

    feat: Add App component

--- 7-folder-structure/goals-todos/src/components/App.js
+++ 7-folder-structure/goals-todos/src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import '../App.css'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+import '../App.css'
+import { handleInitialData } from '../actions/shared'
+import Todos from './Todos'
+import Goals from './Goals'
+
+class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+
+  render() {
+    if (this.props.loading === true) {
+      return <h3>Loading...</h3>
+    }
+
+    return (
+      <div style={{ display: "flex" }}>
+        <Todos />
+        <Goals />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(state => ({
+  loading: state.loading
+}))(App)
+    
